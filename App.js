0ea7aa83690528e37fb40c6e770843da9b765ad3
JON <jonn.thinkrtc@gmail.com>
2018-02-04 15:27:18 -0800

    first commit and config

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/Header';
+import List from './src/components/List';
+
+ class App extends React.Component {
+  render() {
+    return (
+      <View>
+          <Header headerText="SayMosaic" />
+          <List />
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  
+});
+
+
+export default App;
---
+++ src/components/List.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class List extends Component {
+	render(){
+		return(
+				<Text>List</Text>
+			)
+	}
+}
+
+
+export default List;
