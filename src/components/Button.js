74639eb9e8acbbddd1cc9958c3fceea1936a6b4e
JON <jonn.thinkrtc@gmail.com>
2018-02-06 10:48:59 -0800

    updated style for Button Component

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,8 +6,8 @@ import { Text, View, Image, Linking, TouchableOpacity } from 'react-native';
 class Button extends Component {
 	render(){
 		return(
-			<TouchableOpacity>
-				<Text>CLICK ME</Text>
+			<TouchableOpacity style={styles.buttonStyle}>
+				<Text style={styles.textStyle}>CLICK ME</Text>
 			</TouchableOpacity>
 			)
 	}
@@ -16,3 +16,25 @@ class Button extends Component {
 
 
 export default Button;
+
+
+const styles = {
+	textStyle: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#007aff',
+    marginLeft: 5,
+    marginRight: 5
+  }
+}
